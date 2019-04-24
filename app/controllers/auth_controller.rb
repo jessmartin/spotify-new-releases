280b2e609e464ff98db7342e747eaed17db6e3cf
Jess Martin <jessmartin@gmail.com>
2019-04-23 23:38:10 -0400

    Simple OAuth flow

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,5 +6,24 @@ class AuthController < ApplicationController
 
   def callback
     @code = params[:code]
+
+    @access_token = get_access_token(@code)
+
+    session[:access_token] = @access_token
+  end
+
+  private
+
+  def get_access_token(code)
+    auth_response = HTTParty.post("https://accounts.spotify.com/api/token",
+                                  headers: { 'Accept' => 'application/json' },
+                                  body: {
+                                    'client_id' => Rails.application.secrets.spotify_client_id,
+                                    'client_secret' => Rails.application.secrets.spotify_client_secret,
+                                    code: code,
+                                    grant_type: "authorization_code",
+                                    redirect_uri: "http://localhost:3000/callback"
+                                  })
+    auth_response['access_token']
   end
 end
