cad6b8726c884d80ce326de843f3d48e663b2362
Jess Martin <jessmartin@gmail.com>
2019-04-23 23:38:29 -0400

    Improve albums view

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,15 +2,14 @@ require 'httparty'
 
 class ReleasesController < ApplicationController
   def index
-    access_token = ""
+    access_token = session[:access_token]
 
     followed_artists_response = HTTParty.get("https://api.spotify.com/v1/me/following", 
       query: { type: 'artist' },
       headers: {"Authorization" => "Bearer #{access_token}"})
+    @followed_artists = followed_artists_response["artists"]["items"]
 
-    followed_artists = followed_artists_response["artists"]["items"]
-
-    all_albums = followed_artists.collect do |artist|
+    all_albums = @followed_artists.collect do |artist|
       albums_response = HTTParty.get("https://api.spotify.com/v1/artists/#{artist["id"]}/albums", 
                                       headers: {"Authorization" => "Bearer #{access_token}"})
       albums_response["items"]
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/callback', to: 'auth#callback'
+  get '/releases', to: 'releases#index'
 
   root to: 'auth#index'
 end
